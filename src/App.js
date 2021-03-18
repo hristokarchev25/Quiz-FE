24da599c478bf4fac478452241af9af8c2f33eae
Hristo Karchev <x_karchev@mail.bg>
2021-03-18 20:02:15 +0200

    Header.js and Footer.js created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h1>HEYYYY</h1>
+      <Header />
+      <Footer/>
     </div>
   );
 }
