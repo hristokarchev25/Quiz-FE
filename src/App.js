d21edcad51cfcbdd88cb96c5e60bf517584c400c
Hristo Karchev <x_karchev@mail.bg>
2021-03-18 21:25:15 +0200

    CategorieNav added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import Footer from './components/Footer/Footer';
 import Categories from './components/Categories/Categories';
 import './App.css';
 
+
 function App() {
   return (
     <div className="container">
 
       <Header />
+
       <Switch>
-        <Route path="/" component={Categories} />
+        <Route path="/" exact component={Categories} />
+        <Route path="/categories/:category" component={Categories} />
       </Switch>
+
       <Footer />
     </div>
   );
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,19 +1,12 @@
-
+import CategorieNav from './CategorieNav/CategorieNav';
 
 const Categories = () => {
     return (
         <section class="dashboard">
             <h1>Dashboard</h1>
-            <nav class="navbar">
-                <ul>
-                    <li><a href="#">All</a></li>
-                    <li><a href="#">Cats</a></li>
-                    <li><a href="#">Dogs</a></li>
-                    <li><a href="#">Parrots</a></li>
-                    <li><a href="#">Reptiles</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </nav>
+
+            <CategorieNav />
+
             <ul class="other-pets-list">
                 <li class="otherPet">
                     <h3>Name: Gosho</h3>
