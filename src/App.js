7888557083470928ed94b171d1ebc33e5f82da70
Hristo Karchev <x_karchev@mail.bg>
2021-03-22 23:37:29 +0200

    Simple form added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Categories from './components/Categories/Categories';
 import PetDetails from './components/PetDetails/PetDetails';
+import DemoPage from './components/Demo';
 import './App.css';
 
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" exact component={Categories} />
         <Route path="/categories/:category" exact component={Categories} />
         <Route path="/pets/details/:petId" component={PetDetails} />
+        <Route path="/demo" component={DemoPage} />
       </Switch>
 
       <Footer />
