48feac8499ac0b96830c5ad46e4c01323bf22553
Hristo Karchev <x_karchev@mail.bg>
2021-03-25 19:44:36 +0200

    class -> className & for -> htmlFor

--- src/components/CreatePet/CreatePet.js
+++ src/components/CreatePet/CreatePet.js
@@ -19,35 +19,35 @@ const CreatePet = ({
     };
 
     return (
-        <section class="create">
+        <section className="create">
             <form onSubmit={onCreatePetSubmitHandler}>
                 <fieldset>
                     <legend>Add new Pet</legend>
-                    <p class="field">
-                        <label for="name">Name</label>
-                        <span class="input">
+                    <p className="field">
+                        <label htmlFor="name">Name</label>
+                        <span className="input">
                             <input type="text" name="name" id="name" placeholder="Name" />
-                            <span class="actions"></span>
+                            <span className="actions"></span>
                         </span>
                     </p>
-                    <p class="field">
-                        <label for="description">Description</label>
-                        <span class="input">
+                    <p className="field">
+                        <label htmlFor="description">Description</label>
+                        <span className="input">
                             <textarea rows="4" cols="45" type="text" name="description" id="description"
                                 placeholder="Description"></textarea>
-                            <span class="actions"></span>
+                            <span className="actions"></span>
                         </span>
                     </p>
-                    <p class="field">
-                        <label for="image">Image</label>
-                        <span class="input">
+                    <p className="field">
+                        <label htmlFor="image">Image</label>
+                        <span className="input">
                             <input type="text" name="imageURL" id="image" placeholder="Image" />
-                            <span class="actions"></span>
+                            <span className="actions"></span>
                         </span>
                     </p>
-                    <p class="field">
-                        <label for="category">Category</label>
-                        <span class="input">
+                    <p className="field">
+                        <label htmlFor="category">Category</label>
+                        <span className="input">
                             <select type="text" name="category">
                                 <option value="Cat">Cat</option>
                                 <option value="Dog">Dog</option>
@@ -55,10 +55,10 @@ const CreatePet = ({
                                 <option value="Reptile">Reptile</option>
                                 <option value="Other">Other</option>
                             </select>
-                            <span class="actions"></span>
+                            <span className="actions"></span>
                         </span>
                     </p>
-                    <input class="button" type="submit" class="submit" value="Add Pet" />
+                    <input className="button" type="submit" className="submit" value="Add Pet" />
                 </fieldset>
             </form>
         </section>
