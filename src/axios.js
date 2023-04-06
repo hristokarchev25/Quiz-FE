2f0067bd4c3c3bf04c2bbc3e9d6c03d5f721e0a1
Hristo Karchev <73774943+hristokarchev25@users.noreply.github.com>
2023-04-06 22:25:07 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'quizbe-production.up.railway.app/',
+    baseURL: 'https://quizbe-production.up.railway.app/',
     withCredentials: true
 });
 
